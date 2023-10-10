7b09790678d9dd66bb42789e5cc92d5800942fee
Clément Besnier <clem@clementbesnier.fr>
2023-10-10 23:33:12 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# CLTK with Stanza
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='cltk-with-stanza',
+    version='0.0.1',
+    packages=['cltk_with_stanza'],
+    url='http://cltk.org',
+    license='MIT',
+    author='Clément Besnier',
+    author_email='clem@clementbesnier.fr',
+    description='CLTK wrapper for Stanza'
+)
